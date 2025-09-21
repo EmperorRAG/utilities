220c64f75f66f6aa023d0baceacb2ea734e82e2d
EmperorRAG <mrbrewermj@gmail.com>
2025-09-21 23:15:01 +0200

    refactor: simplify toString function overloads and improve documentation

--- src/lib/helper-functions/toString.utils.ts
+++ src/lib/helper-functions/toString.utils.ts
@@ -3,39 +3,27 @@ import { isValueObject } from '../types/object/object.types';
 import type { Stringable } from '../types/stringable/stringable.types';
 
 /**
- * Converts a value or array of values that are "Stringable" to their string representations.
- *
- * - If the input is an array, each element is converted to a string using `String()`, unless it is a plain object,
- *   in which case it is stringified using `JSON.stringify()`.
- * - If the input is a single value, it is converted to a string using `String()` or `JSON.stringify()` for plain objects.
- *
- * @param value - The value or array of values to convert. Must be a `Stringable` or an array of `Stringable` values.
- *   - `Stringable` is typically a string, number, boolean, or an object with a custom `toString()` method.
- * @returns
- *   - If the input is an array, returns an array of strings with each element converted.
- *   - If the input is a single value, returns its string representation.
- *
- * @remarks
- * - If an object has a custom `toString()` method (not the default from `Object.prototype`), it will be used.
- * - If an object does not have a custom `toString()`, it will be stringified using `JSON.stringify()`.
- * - For primitive values, `String(value)` is used.
- *
- * @example
- * ```ts
- * toString(42); // "42"
- * toString([1, 2, 3]); // ["1", "2", "3"]
- * toString({ foo: "bar" }); // '{"foo":"bar"}'
- * toString([{ foo: "bar" }, 7]); // ['{"foo":"bar"}', "7"]
- * ```
- */
-export function toString(value: Stringable): string;
-/**
  * Converts an array of Stringable values to an array of strings.
  *
- * @param value - An array of values to convert.
- * @returns An array of string representations.
+ * @param value - An array of values to convert. Each element should be a Stringable (string, number, boolean, or object with a custom toString method).
+ * @returns An array of string representations for each element.
+ *
+ * @example
+ * toString([1, 2, 3]); // ["1", "2", "3"]
+ * toString([{ foo: "bar" }, 7]); // ['{"foo":"bar"}', "7"]
  */
 export function toString(value: Stringable[]): string[];
+/**
+ * Converts a single Stringable value to its string representation.
+ *
+ * @param value - The value to convert. Should be a string, number, boolean, or object with a custom toString method.
+ * @returns The string representation of the value.
+ *
+ * @example
+ * toString(42); // "42"
+ * toString({ foo: "bar" }); // '{"foo":"bar"}'
+ */
+export function toString(value: Stringable): string;
 /**
  * Converts a Stringable value or an array of Stringable values to their string representations.
  *
